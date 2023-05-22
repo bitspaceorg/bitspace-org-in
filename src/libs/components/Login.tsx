2f01f65dea3c79e6390af74ea48d811435584303
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-05-22 09:04:55 +0530

    UI changes

---
+++ src/libs/components/Login.tsx
@@ -0,0 +1,13 @@
+const Login = () => {
+
+    return (<>
+        <div className="flex absolute h-[100vh] w-[100vw] bg-[rgba(0,0,0,0.7)] items-center justify-center text-white">
+            <div className="w-[35%] h-3/4 border rounded">
+                Login
+            </div>
+        </div>
+    </>)
+
+}
+
+export default Login;
--- src/libs/components/Navbar.tsx
+++ src/libs/components/Navbar.tsx
@@ -1,5 +1,7 @@
 'use client'
+import Login from '@/libs/components/Login'
 import {useRouter, usePathname} from "next/navigation"
+import { useState } from 'react'
 
 const Navbar = () => {
 
@@ -7,28 +9,37 @@ const Navbar = () => {
         { name: 'Newsletters', href: '/news' },
         { name: 'Articles', href: '/articles' },
         { name: 'Courses', href: '/courses' },
-        { name: 'Steaming', href: '/streams' },
+        { name: 'Home', href: '/' },
+        { name: 'Streaming', href: '/streams' },
         { name: 'Forum', href: '/forum' },
         { name: 'FAQ', href: '/faq' },
-        { name: 'Home', href: '/' },
     ]
+
+    const[popup,setPopup] = useState(false);
 
     const router = useRouter()
     const pathname = usePathname()
 
+    const authenticate = () => {
+        setPopup(true)
+    }
+
     return (
+        <>
+        {popup && <Login/>}
         <div className="bg-black h-[5rem] flex justify-between items-center">
-        <div>
         <span className='mx-5 text-4xl font-bold text-white'>:bs</span>
+        <div>
             { navLinks.map((link) => {
                 const isActive:boolean = pathname === link.href
                 return (
-                   <button key={link.name} onClick={() => router.push(link.href)} className={isActive ? 'transition-all scale-[1.35] p-2 m-4 font-bold text-[#9333EA] hover:text-[#9333EA]' : 'transition-all hover:scale-[1.25] p-2 m-4 font-bold text-[#989898] hover:text-white'}>{link.name}</button>
+                   <button key={link.name} onClick={() => router.push(link.href)} className={isActive ? ' rounded-md border border-[#9333EA] transition-all scale-[1.35] p-2 m-4 font-bold text-[#9333EA] hover:text-[#9333EA]' : 'transition-all hover:scale-[1.25] p-2 m-4 font-bold text-[#989898] hover:text-white'}>{link.name}</button>
                 )
             })}
         </div>
-            <button className="text-black rounded-xl bg-[#9333EA] px-5 py-2 font-bold hover:text-white transition-all mx-5">Login</button>
+            <button className="text-black rounded-xl bg-[#9333EA] px-5 py-2 font-bold hover:text-white transition-all mx-5" onClick={authenticate}>Login</button>
         </div>
+        </>
 
     )
 }

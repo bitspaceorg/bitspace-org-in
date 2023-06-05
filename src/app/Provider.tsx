3c1af406cb61a71507c8a406e28dce2243ebfe43
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-06-05 15:50:06 +0530

    Session TypeScript error solved

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from "next-auth/react"
 
-const NextAuthProvider = ({children}) => {
+const NextAuthProvider = ({children} : any) => {
     return(
         <SessionProvider>{children}</SessionProvider>
     )
